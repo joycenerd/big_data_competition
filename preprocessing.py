6f1a9b2bb69820ca8b35f968a832795d25ab53f6
pony <pony8674312@gmail.com>
2019-09-19 10:51:26 +0800

    add boxplot

--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from matplotlib import pyplot as plt
+import seaborn as sns
 
 
 data_path = "D:/VScode workshop/big_data_competition/data/"
@@ -18,11 +19,12 @@ for i , j in bins :
 			bin_col.append(k)
 	for k in bin_col : 
 		sub_df[k].replace({'Y' : 1 , 'N' : 0} , inplace = True)
-	#print(sub_df.corr())
+	print(sub_df.corr())
 
 #|r| >= 0.05 (相關係數前十大)
 #X_B_IND、X_C_IND、X_E_IND、X_H_IND、TOOL_VISIT_1YEAR_CNT、DIEBENEFIT_AMT、
 #DIEACCIDENT_AMT、MONTHLY_CARE_AMT、LIFE_INSD_CNT、IF_ISSUE_INSD_I_IND
+
 
 bin_col = []
 for i in train_df.columns.to_list() : 
@@ -44,3 +46,9 @@ for i in r_col :
 	plt.ylabel(i)
 	plt.title("EDA scatter")
 	plt.show()
+
+for i in r_col : 
+	if train_df[i].nunique() > 2 : 
+		sns.boxplot(x = 'Y1' , y = i , data = train_df , palette = "hls")
+		plt.show()
+
