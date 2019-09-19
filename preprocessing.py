3b342614c103d4251bc95c76ec282847ea029564
pony <pony8674312@gmail.com>
2019-09-19 23:06:23 +0800

    optimize prerpocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -6,26 +6,7 @@ import sklearn
 
 
 data_path = "D:/VScode workshop/big_data_competition/data/"
-train_df = pd.read_csv(data_path + "train.csv" , encoding = "big5")
-
-
-bins = [(72 , 84) , (84 , 91) , (91 , 97) , (97 , 103) , (103 , 109)]
-
-for i , j in bins : 
-	sub_df = train_df.iloc[ : , i : j]
-	sub_df = pd.concat([sub_df , train_df[['Y1']]] , axis = 1)
-	bin_col = []
-	for k in sub_df.columns.to_list() : 
-		if sub_df[k].nunique() == 2 : 
-			bin_col.append(k)
-	for k in bin_col : 
-		sub_df[k].replace({'Y' : 1 , 'N' : 0} , inplace = True)
-	print(sub_df.corr())
-
-#|r| >= 0.05 (相關係數前十大)
-#X_B_IND、X_C_IND、X_E_IND、X_H_IND、TOOL_VISIT_1YEAR_CNT、DIEBENEFIT_AMT、
-#DIEACCIDENT_AMT、MONTHLY_CARE_AMT、LIFE_INSD_CNT、IF_ISSUE_INSD_I_IND
-
+train_df = pd.read_csv(data_path + "train.csv" , encoding = "big5" , low_memory = False)
 
 bin_col = []
 for i in train_df.columns.to_list() : 
@@ -34,10 +15,18 @@ for i in train_df.columns.to_list() :
 for i in bin_col : 
 	train_df[i].replace({'Y' : 1 , 'N' : 0} , inplace = True)
 
+sub_df = train_df.iloc[ : , 72 : 109]
+sub_df = pd.concat([sub_df , train_df[['Y1']]] , axis = 1)
+print(sub_df.corr()['Y1'])
+
+#|r| >= 0.05 (相關係數前十大)
+#X_B_IND、X_C_IND、X_E_IND、X_H_IND、TOOL_VISIT_1YEAR_CNT、DIEBENEFIT_AMT、
+#DIEACCIDENT_AMT、MONTHLY_CARE_AMT、LIFE_INSD_CNT、IF_ISSUE_INSD_I_IND
+
 r_col = ["X_B_IND" , "X_C_IND" , "X_E_IND" , "X_H_IND" , "TOOL_VISIT_1YEAR_CNT" , "DIEBENEFIT_AMT" , 
 "DIEACCIDENT_AMT" , "MONTHLY_CARE_AMT" , "LIFE_INSD_CNT" , "IF_ISSUE_INSD_I_IND"]
 
-
+'''
 for i in r_col : 
 	plt.scatter(range(100000) , train_df[i])
 	plt.xlabel("count")
@@ -49,4 +38,4 @@ for i in r_col :
 	if train_df[i].nunique() > 2 : 
 		sns.boxplot(x = 'Y1' , y = i , data = train_df , palette = "hls")
 		plt.show()
-
+'''
