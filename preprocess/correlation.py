0eda28ad519f443e9192c19aea46aa17a564f6a0
Kyle Huang <kyle.huang168@gmail.com>
2019-09-20 21:57:46 +0800

    solve out error and add scatter

--- preprocess/correlation.py
+++ preprocess/correlation.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 trainPath = "../data/train.csv"
 
@@ -21,11 +23,16 @@ df1 = pd.concat([df1, train_df[['Y1']]], axis=1)
 for k in df1.columns.to_list():
     if df1[k].nunique() == 2:
         df1[k].replace({'M' : 1, 'F' : 0, 'Y' : 1 ,'N' : 0}, inplace=True)
-    if df1[k].nunique() == 4:
-        df1[k].replace({'低':0, '中低':1, '中高':2, '高': 3}, inplace=True)
+    if (df1[k].nunique() == 4 )& (df1[k].dtype == object):
+        df1[k].replace({'低':0, '中':1, '中高':2, '高': 3}, inplace=True)
 
+# n = 0
+# while n < 36:
+#     print(df1.iloc[:,n : n + 4])
+#     n += 4
+corr = df1.corr()['Y1']
 
-print(df1.iloc[:, 17:18])
-# print(df1.corr()['Y1'])
+plt.scatter(corr, range(corr.shape[0]))
+plt.show()
 
 
